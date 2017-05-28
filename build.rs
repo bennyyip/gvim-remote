787cb9b471a5640f55a4b1d3ad8e7e52baaa9459
BennyYip <yebenmy@gmail.com>
2017-05-29 01:00:04 +0800

    cargo fmt

--- build.rs
+++ build.rs
@@ -3,9 +3,9 @@
 extern crate winres;
 
 fn main() {
-  if cfg!(target_os = "windows") {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("vim48x48.ico");
-    res.compile().unwrap();
-  }
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("vim48x48.ico");
+        res.compile().unwrap();
+    }
 }

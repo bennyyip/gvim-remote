37fb134e7cdced33012e8d403602a9aba0ee2b87
BennyYip <yebenmy@gmail.com>
2017-05-30 15:20:00 +0800

    use hi-res icon

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ extern crate winres;
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("vim48x48.ico");
+        res.set_icon("vim.ico");
         res.compile().unwrap();
     }
 }
